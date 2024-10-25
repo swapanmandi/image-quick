7625a9ccb7570efd7288a29a691a49ebd70306fd
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-10-25 20:05:09 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+
+        <ul className=' flex justify-around'>
+            <li>Home</li>
+            <li>Image to Pdf</li>
+            <li>Resize Image</li>
+            <li>Crop Image</li>
+        </ul>
+    </div>
+  )
+}
