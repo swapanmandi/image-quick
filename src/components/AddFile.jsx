b37c1c1654c10be1217aadfe7401179b4edc1e92
SWAPAN MANDI <swapanmandi5@gmail.com>
2025-02-04 12:55:02 +0530

    hide  add container when aded file

--- src/components/AddFile.jsx
+++ src/components/AddFile.jsx
@@ -22,18 +22,25 @@ export default function AddFile() {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div className=" w-full flex justify-center m-2 mb-4" {...getRootProps()}>
-      <input {...getInputProps()} />
-      <div className=" flex justify-center bg-darkPalette-300 w-full min-h-40 p-2 lg:w-1/2 rounded-md items-center">
-        {isDragActive ? (
-          <p>Drop the files here ...</p>
-        ) : (
-          <p>
-            Drag 'n' drop file(s) here, or{" "}
-            <span className=" font-semibold">click</span> to select file(s)
-          </p>
-        )}
-      </div>
-    </div>
+    <>
+      {orgImagePath.length == 0 && (
+        <div
+          className=" w-full flex justify-center m-2 mb-4"
+          {...getRootProps()}
+        >
+          <input {...getInputProps()} />
+          <div className=" flex justify-center bg-darkPalette-300 w-full min-h-40 p-2 lg:w-1/2 rounded-md items-center">
+            {isDragActive ? (
+              <p>Drop the files here ...</p>
+            ) : (
+              <p>
+                Drag 'n' drop file(s) here, or{" "}
+                <span className=" font-semibold">click</span> to select file(s)
+              </p>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
