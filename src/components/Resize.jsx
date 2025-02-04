114d2c215bfca042c38d6be135e39e9824c17ef2
SWAPAN MANDI <swapanmandi5@gmail.com>
2025-02-04 11:47:43 +0530

    update the style

--- src/components/Resize.jsx
+++ src/components/Resize.jsx
@@ -15,14 +15,14 @@ export default function Resize() {
   const sizeType = useSelector((state) => state.imageEditing.sizeType);
 
   const sizeTypes = [
-    { label: "pixel", value: "pixel" },
-    { label: "percentage", value: "percentage" },
+    { label: "Pixel", value: "pixel" },
+    { label: "Percentage", value: "percentage" },
   ];
 
   return (
     <div className=" w-full flex justify-center">
       <div className=" p-4 space-y-2 lg:w-1/2">
-        <div className="flex items-center lg:mb-4">
+        <div className="  flex justify-center items-center lg:mb-4">
           {sizeTypes.map((item) => (
             <label className=" mr-2" key={item.label}>
               <input
@@ -37,26 +37,30 @@ export default function Resize() {
           ))}
         </div>
 
-        <div className=" flex justify-between lg:justify-start">
-          <p className="mr-2">Width:</p>
-          <input
-            className=" bg-slate-900 text-white w-28 mr-2  outline-none p-1 rounded-md lg:mr-8"
-            type="number"
-            onChange={(e) => dispatch(setResizedWidth(e.target.value))}
-            value={resizedWidth}
-            placeholder={sizeType === "pixel" ? "pixel" : " %"}
-            required
-          ></input>
+        <div className=" flex justify-between lg:justify-around">
+          <div className="">
+            <label className="mr-2">Width:</label>
+            <input
+              className=" bg-slate-900 text-white w-14 lg:w-28 mr-2  outline-none p-1 rounded-md lg:mr-8"
+              type="number"
+              onChange={(e) => dispatch(setResizedWidth(e.target.value))}
+              value={resizedWidth}
+              placeholder={sizeType === "pixel" ? "Pixel" : " %"}
+              required
+            ></input>
+          </div>
 
-          <p className=" mr-2">Height:</p>
-          <input
-            className=" bg-slate-900 w-28 text-white outline-none p-1 rounded-md"
-            type="number"
-            onChange={(e) => dispatch(setResizedHeight(e.target.value))}
-            value={resizedHeight}
-            placeholder={sizeType === "pixel" ? "pixel" : " %"}
-            required
-          ></input>
+          <div className="">
+            <label className=" mr-2">Height:</label>
+            <input
+              className=" bg-slate-900 w-14 lg:w-28 text-white outline-none p-1 rounded-md"
+              type="number"
+              onChange={(e) => dispatch(setResizedHeight(e.target.value))}
+              value={resizedHeight}
+              placeholder={sizeType === "pixel" ? "Pixel" : " %"}
+              required
+            ></input>
+          </div>
         </div>
       </div>
     </div>
