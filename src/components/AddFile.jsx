c434b04cbb80afee5e352ff0a77e52d290d21934
SWAPAN MANDI <swapanmandi5@gmail.com>
2025-02-04 09:53:00 +0530

    center the text on add file

--- src/components/AddFile.jsx
+++ src/components/AddFile.jsx
@@ -3,7 +3,6 @@ import { useDropzone } from "react-dropzone";
 import { useDispatch, useSelector } from "react-redux";
 import { setOrgImagePath } from "../store/imageSlice.js";
 
-
 export default function AddFile() {
   const dispatch = useDispatch();
   const orgImagePath = useSelector((state) => state.imageEditing.orgImagePath);
@@ -22,19 +21,19 @@ export default function AddFile() {
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
-//console.log("org path", orgImagePath)
-
   return (
     <div className=" w-full flex justify-center m-2 mb-4" {...getRootProps()}>
       <input {...getInputProps()} />
-      <div className=" bg-darkPalette-300 w-full min-h-40 p-2 lg:w-1/2 rounded-md">
+      <div className=" flex justify-center bg-darkPalette-300 w-full min-h-40 p-2 lg:w-1/2 rounded-md items-center">
         {isDragActive ? (
           <p>Drop the files here ...</p>
         ) : (
-          <p>Drag 'n' drop file(s) here, or <br/> <span className=" font-semibold">click</span> to select file(s)</p>
+          <p>
+            Drag 'n' drop file(s) here, or{" "}
+            <span className=" font-semibold">click</span> to select file(s)
+          </p>
         )}
       </div>
-     
     </div>
   );
 }
