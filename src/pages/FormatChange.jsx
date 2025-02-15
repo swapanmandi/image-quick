5f8a6ef50d32a6b33e329e695c3d099b49b77c5b
SWAPAN MANDI <swapanmandi5@gmail.com>
2025-02-16 01:22:55 +0530

    refactor FormatChange component for improved readability and layout

--- src/pages/FormatChange.jsx
+++ src/pages/FormatChange.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Download from "../components/Download";
 import AddFile from "../components/AddFile";
 import DisplayImage from "../components/DisplayImage";
@@ -9,7 +9,9 @@ export default function FormatChange() {
   const dispatch = useDispatch();
   const orgImagePath = useSelector((state) => state.imageEditing.orgImagePath);
   const format = useSelector((state) => state.imageEditing.format);
- const editedImagePath = useSelector(state=> state.imageEditing.editedImagePath)
+  const editedImagePath = useSelector(
+    (state) => state.imageEditing.editedImagePath
+  );
 
   const handleFormatChange = () => {
     orgImagePath.forEach((path) => {
@@ -33,35 +35,45 @@ export default function FormatChange() {
             { filePath: formatedImg, fileSize: formatedImageSizeInKb },
           ])
         );
-       
       };
     });
   };
 
   useEffect(() => {
-      return () => {
-        //dispatch(clearOrgImagePath());
-        dispatch(clearEditedImagePath());
-      };
-    }, []);
+    return () => {
+      //dispatch(clearOrgImagePath());
+      dispatch(clearEditedImagePath());
+    };
+  }, []);
 
   return (
     <div className=" w-full p-3">
       <div>
-      <h1 className=" text-2xl">Image Format Change</h1>
-      <p>
-        Change your image format to a custom format in a few simple steps. Upload one
-        or more images, click "Change Format" button and download your file.
-      </p>
-      <p>Supported Images: JPG, JPEG, PNG, WEBP, SVG</p>
+        <h1 className=" text-2xl place-self-center">Image Format Change</h1>
+
         <AddFile />
         <DisplayImage />
         <Download />
       </div>
-      {
-        orgImagePath.length > 0 && editedImagePath.length == 0 && <div className=" flex justify-center"><button className=" bg-darkPalette-400 p-1 px-2 rounded-md text-black" onClick={handleFormatChange}>Change Format</button></div>
-      }
-      
+      {orgImagePath.length > 0 && editedImagePath.length == 0 && (
+        <div className=" flex justify-center">
+          <button
+            className=" bg-darkPalette-400 p-1 px-2 rounded-md text-black"
+            onClick={handleFormatChange}
+          >
+            Change Format
+          </button>
+        </div>
+      )}
+
+      <div className=" w-full flex flex-col justify-center items-center pt-14">
+        <p>
+          Change your image format to a custom format in a few simple steps.
+          Upload one or more images, click "Change Format" button and download
+          your file.
+        </p>
+        <p>Supported Images: JPG, JPEG, PNG, WEBP, SVG</p>
+      </div>
     </div>
   );
 }
