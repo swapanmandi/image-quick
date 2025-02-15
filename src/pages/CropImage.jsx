50c76eb50673d37b673a01fa280072686110cda6
SWAPAN MANDI <swapanmandi5@gmail.com>
2025-02-16 01:22:50 +0530

    improve layout in CropImage component for better content organization

--- src/pages/CropImage.jsx
+++ src/pages/CropImage.jsx
@@ -8,16 +8,19 @@ import Crop from "../components/Crop";
 export default function CropImage() {
   return (
     <div className=" p-3">
-      <h1 className=" text-2xl"> Resize Image</h1>
-      <p>
-        Crop your images to a custom size in a few simple steps. Upload one or
-        more images, click "Crop" button and download your file.
-      </p>
-      <p>Supported Images: JPG, JPEG, PNG, WEBP, SVG</p>
+      <h1 className=" text-2xl place-self-center"> Resize Image</h1>
+
       <AddFile />
-      {/* <DisplayImage /> */}
       <Crop />
       <Download />
+
+      <div className=" w-full flex flex-col justify-center items-center pt-14">
+        <p>
+          Crop your images to a custom size in a few simple steps. Upload one or
+          more images, click "Crop" button and download your file.
+        </p>
+        <p>Supported Images: JPG, JPEG, PNG, WEBP, SVG</p>
+      </div>
     </div>
   );
 }
