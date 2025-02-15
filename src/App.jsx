7c638eb87654e43f140638336cecb998a5fc08a0
SWAPAN MANDI <swapanmandi5@gmail.com>
2025-02-16 01:23:19 +0530

    add FeaturesSection to App component and adjust layout for improved visibility

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import "./App.css";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer.jsx";
+import FeaturesSection from "./components/FeaturesSection.JSX";
 
 function App() {
   return (
     <div className=" bg-darkPalette-200 h-svh">
       <Header />
-      <div className=" min-h-[80%]">
+      <div className=" min-h-[60vh]">
         <Outlet />
       </div>
-
+      <FeaturesSection />
+      <p className=" text-sm place-self-center p-4">
+        Made with ❤️ by <a href="">Swapan Mandi</a>
+      </p>
       <Footer />
     </div>
   );
