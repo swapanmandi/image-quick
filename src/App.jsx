570094d544892f295130a9e53d167a2af100b87c
SWAPAN MANDI <swapanmandi5@gmail.com>
2025-02-17 23:14:44 +0530

    update App component: adjust minimum height for better layout and update author link

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ function App() {
   return (
     <div className=" bg-darkPalette-200 h-svh">
       <Header />
-      <div className=" min-h-[60vh]">
+      <div className=" min-h-[40vh] lg:min-h-[60vh]">
         <Outlet />
       </div>
       <FeaturesSection />
       <p className=" text-sm place-self-center p-4">
-        Made with ❤️ by <a href="">Swapan Mandi</a>
+        Made with ❤️ by <a href="https://github.com/swapanmandi">Swapan Mandi</a>
       </p>
       <Footer />
     </div>
