043c5248ceaff84d621978f5213f446000a28552
SWAPAN MANDI <swapanmandi5@gmail.com>
2025-02-22 19:46:20 +0530

    refactor(CropImage): remove unused imports for cleaner code

--- src/pages/CropImage.jsx
+++ src/pages/CropImage.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import AddFile from "../components/AddFile";
-import DisplayImage from "../components/DisplayImage";
 import Download from "../components/Download";
 import Crop from "../components/Crop";
 
